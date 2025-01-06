42537b2b771325785dc11a9cc3825eee494a9247
Muhammad Zeeshan <147901779+MZeeshanMalik@users.noreply.github.com>
2025-01-06 16:11:41 +0500

    Update axiosBaseUrl.js

--- client/src/app/axiosBaseUrl.js
+++ client/src/app/axiosBaseUrl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // Server URL
-const axiosBaseUrl = axios.create({ baseURL: "http://13.233.151.147/api" });
+const axiosBaseUrl = axios.create({ baseURL: "https://13.233.151.147/api" });
 
 export default axiosBaseUrl;
